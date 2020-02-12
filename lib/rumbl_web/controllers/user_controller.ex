86cb639983932f6d623719492d60878382384e35
Fernando Castellanos <fernando.castellanos@gmail.com>
2020-02-12 07:40:04 -0800

    Authentication WIP

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -5,9 +5,13 @@ defmodule RumblWeb.UserController do
     alias Rumbl.Accounts.User
 
     def index(conn, _params) do
-        users = Accounts.list_users()
-
-        render(conn, "index.html", users: users)
+        case authenticate(conn) do
+            %Plug.Conn{halted: true} = conn ->
+                conn
+            conn ->
+                users = Accounts.list_users()
+                render(conn, "index.html", users: users)
+        end
     end
 
     def show(conn, %{"id" => id}) do
@@ -17,19 +21,30 @@ defmodule RumblWeb.UserController do
     end
 
     def new(conn, _params) do
-        changeset = Accounts.change_user(%User{})
+        changeset = Accounts.change_registration(%User{}, %{})
 
         render(conn, "new.html", changeset: changeset)
     end
 
     def create(conn, %{"user" => user_params}) do
-        case Accounts.create_user(user_params) do
+        case Accounts.register_user(user_params) do
             {:ok, user} ->
                 conn
                 |> put_flash(:info, "#{user.name} created!")
                 |> redirect(to: Routes.user_path(conn, :index))
             {:error, %Ecto.Changeset{} = changeset} ->
                 render(conn, "new.html", changeset: changeset)
+        end
+    end
+
+    defp authenticate(conn) do
+        if conn.assigns.current_user do
+            conn
+        else
+            conn
+            |> put_flash(:error, "You must be logged in to access that page")
+            |> redirect(to: Routes.page_path(conn, :index))
+            |> halt()
         end
     end
 end
